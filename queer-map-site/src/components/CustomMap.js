d74796b49e346ef8fc7cd7031f89b44f25fc3f25
alexrenn <106498251+alexrenn@users.noreply.github.com>
2025-02-01 22:14:47 -0800

    default location is where you're at

--- queer-map-site/src/components/CustomMap.js
+++ queer-map-site/src/components/CustomMap.js
@@ -1,24 +1,48 @@
-import React from "react";
-import { Map, Marker} from "@vis.gl/react-google-maps";
-
+import React, { useState, useEffect } from "react";
+import { Map, Marker } from "@vis.gl/react-google-maps";
 import "../styles/HomePage.css";
 
 function CustomMap() {
-  const markerLocation = {
-    lat: 51.509865,
-    lng: -0.118092,
-  };
+  const [location, setLocation] = useState({
+    lat: 51.509865,  // Default location (London)
+    lng: -0.118092,  // Default location (London)
+  });
+
+  const [mapCenter, setMapCenter] = useState(location);
+
+
+  // Get the current geolocation when the component mounts
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          const newLocation = ({
+            lat: latitude,
+            lng: longitude,
+          });
+          setLocation(newLocation);
+          setMapCenter(newLocation);  // Update the map center with the user's location
+        },
+        () => {
+          console.error("Geolocation error");
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);  // Empty dependency array to run only once when the component mounts
 
   return (
     <div className="map-container">
       <Map
         style={{ borderRadius: "20px" }}
         defaultZoom={13}
-        defaultCenter={markerLocation}
+        defaultCenter={mapCenter}
         gestureHandling={"greedy"}
         disableDefaultUI
       >
-        <Marker position={markerLocation} />
+        <Marker position={location} />
       </Map>
     </div>
   );
